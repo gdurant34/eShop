953857508fa3c4e21151dd5b91aa4c11e08202ff
Grace Durant <gdurant34@gmail.com>
2024-01-03 20:15:43 -0800

    installed material UI, added icons to header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
-import './App.css';
+import "./App.css";
+import ShoppingBasketIcon from "@mui/icons-material/ShoppingBasket";
+import StorefrontIcon from "@mui/icons-material/Storefront";
 
 function App() {
   return (
     <div className="App">
-
+      <div className="header_logo">
+        <StorefrontIcon className="header_logoImage" fontSize="large" />
+        <h2 className="header_logoTitle">eShop</h2>
+      </div>
+      <div className="header_search"></div>
+      <div className="header_nav">
+        <div className="nav_item">
+          <span className="nav_itemLineOne">Hello, Guest</span>
+          <span className="nav_itemLineTwo">Sign In</span>
+        </div>
+        <div className="nav_item">
+          <span className="nav_itemLineOne">Your</span>
+          <span className="nav_itemLineTwo">Shop</span>
+        </div>
+        <div className="nav_item">
+          <ShoppingBasketIcon />
+          <span className="nav_itemLineTwo">0</span>
+        </div>
+      </div>
     </div>
   );
 }
