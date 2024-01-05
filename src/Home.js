82faea69cebec91a2623acf61a4c847b6a522f4d
Grace Durant <gdurant34@gmail.com>
2024-01-04 17:39:47 -0800

    fixed most the CSS problems

--- src/Home.js
+++ src/Home.js
@@ -4,28 +4,63 @@ import Image from "./ecommerce-mcommerce-featured-image-5fd09a3a5ff2a-1520x800.w
 import Product from "./Product";
 
 const Home = () => {
-    return (
-        <div>
-            <div className="home">
-                <div className="home_container">
-                    <img src={Image} alt="" className="home_image" />
-                    <div className="home_row">
-                        <Product />
-                        <Product />
-                    </div>
-                    <div className="home_row">
-                        <Product />
-                        <Product />
-                        <Product />
-                    </div>
-                    <div className="home_row">
-                        <Product />
-                    </div>
-
-                </div>
-            </div>
+  return (
+    <div>
+      <div className="home">
+        <div className="home_container">
+          <img src={Image} alt="" className="home_image" />
+          <div className="home_row">
+            <Product
+              id="1"
+              title="YODALA Vintage Laptop Backpack for women men, Water Resistant Cool BookBag Computer Bag with USB Charging Port for Work Travel College, Fits up to 15.6Inch Notebook in Green"
+              image="https://images-na.ssl-images-amazon.com/images/I/71mEsHyzSCL._SL1000_.jpg"
+              price={42}
+              rating={5}
+            />
+            <Product
+              id="2"
+              title="YODALA Vintage Laptop Backpack for women men, Water Resistant Cool BookBag Computer Bag with USB Charging Port for Work Travel College, Fits up to 15.6Inch Notebook in Green"
+              image="https://images-na.ssl-images-amazon.com/images/I/71mEsHyzSCL._SL1000_.jpg"
+              price={42}
+              rating={5}
+            />
+          </div>
+          <div className="home_row">
+            <Product
+              id="3"
+              title="YODALA Vintage Laptop Backpack for women men, Water Resistant Cool BookBag Computer Bag with USB Charging Port for Work Travel College, Fits up to 15.6Inch Notebook in Green"
+              image="https://images-na.ssl-images-amazon.com/images/I/71mEsHyzSCL._SL1000_.jpg"
+              price={42}
+              rating={4}
+            />
+            <Product
+              id="4"
+              title="YODALA Vintage Laptop Backpack for women men, Water Resistant Cool BookBag Computer Bag with USB Charging Port for Work Travel College, Fits up to 15.6Inch Notebook in Green"
+              image="https://images-na.ssl-images-amazon.com/images/I/71mEsHyzSCL._SL1000_.jpg"
+              price={42}
+              rating={5}
+            />
+            <Product
+              id="5"
+              title="YODALA Vintage Laptop Backpack for women men, Water Resistant Cool BookBag Computer Bag with USB Charging Port for Work Travel College, Fits up to 15.6Inch Notebook in Green"
+              image="https://images-na.ssl-images-amazon.com/images/I/71mEsHyzSCL._SL1000_.jpg"
+              price={42}
+              rating={2}
+            />
+          </div>
+          <div className="home_row">
+            <Product
+              id="6"
+              title="YODALA Vintage Laptop Backpack for women men, Water Resistant Cool BookBag Computer Bag with USB Charging Port for Work Travel College, Fits up to 15.6Inch Notebook in Green"
+              image="https://images-na.ssl-images-amazon.com/images/I/71mEsHyzSCL._SL1000_.jpg"
+              price={42}
+              rating={4}
+            />
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default Home;
--- src/Product.js
+++ src/Product.js
@@ -1,27 +1,25 @@
 import React from "react";
 import "./Product.css";
 
-const Product = () => {
+const Product = ({ title, image, price, rating }) => {
   return (
-    <div>
-      <div className="product">
-        <div className="product_info">
-          <p>Title</p>
-          <p className="product_price">
-            <small>$</small>
-            <strong>30</strong>
-          </p>
-          <div className="product_rating">⭐️⭐️⭐️⭐️</div>
-          <img
-            // src="https://img.kwcdn.com/product/Fancyalgo/VirtualModelMatting/4d2532083d1f9f15b0b9e81f52af2f6f.jpg?imageView2/2/w/800/q/70"
-            src=""
-            alt=""
-            // style={{ maxHeight: '200px' }}
-
-          />
-          <button>Add to Basket</button>
+    <div className="product">
+      <div className="product_info">
+        <p>{title}</p>
+        <p className="product_price">
+          <small>$</small>
+          <strong>{price}</strong>
+        </p>
+        <div className="product_rating">
+          {Array(rating)
+            .fill()
+            .map((_, i) => (
+              <p>⭐️</p>
+            ))}
         </div>
       </div>
+      <img src={image} alt="" />
+      <button>Add to Basket</button>
     </div>
   );
 };
