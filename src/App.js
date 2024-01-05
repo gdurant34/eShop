d0d758e5ca818f0551cbdce00cd58bd2b670fc57
Grace Durant <gdurant34@gmail.com>
2024-01-04 18:15:21 -0800

    added react router dom and routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import "./App.css";
+import Checkout from "./Checkout";
 import Header from "./Header";
 import Home from "./Home";
-
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Home />
+            <Header />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} ></Route>
+          <Route path="/checkout" element={<Checkout />} ></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
+
 
 export default App;
