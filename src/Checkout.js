a883b09857d9ffa4321c3d221c8355d4c23bad03
Grace Durant <gdurant34@gmail.com>
2024-01-04 19:51:43 -0800

    added checkout page and styling

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,10 +1,25 @@
 import React from "react";
 import "./Checkout.css";
+import CheckoutProduct from "./CheckoutProduct";
+import Subtotal from "./Subtotal";
 
 const Checkout = () => {
   return (
-    <div>
-        <h1>Hello world</h1>
+    <div className="checkout">
+      <div className="checkout_left">
+        <img
+          src="https://www.webfx.com/wp-content/uploads/2021/10/banner-ad-example-online.png"
+          alt=""
+          className="checkout_ad"
+        />
+        <div>
+          <h2 className="checkout_title">Your Shopping Basket</h2>
+          <CheckoutProduct />
+        </div>
+      </div>
+      <div className="checkout_right">
+        <Subtotal />
+      </div>
     </div>
   );
 };
